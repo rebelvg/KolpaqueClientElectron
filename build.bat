02e7200e88dc23bd0bef20d777bd20c13e8bf6d1
Rebel <johnhartigan@ya.ru>
2017-03-30 11:23:14 +0300

    no message

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-electron-packager ./ --out=c:/KolpaqueClientElectron --platform=win32,linux,darwin --arch=ia32,x64 --icon=./icon.ico --overwrite
+electron-packager ./ --out=c:/KolpaqueClientElectron --platform=win32,darwin,linux --arch=ia32,x64 --icon=./icon --win32metadata.ProductName=KolpaqueClientElectron --win32metadata.InternalName=KolpaqueClientElectron --win32metadata.FileDescription=KolpaqueClientElectron --win32metadata.OriginalFilename=KolpaqueClientElectron.exe --ignore=.idea --overwrite
 pause
