ef990fa6d65c0acdad79f9a07131dea612408514
Jack Pyro <PyroLovesBuns@gmail.com>
2017-10-03 23:21:52 +0300

    Context menu initial commit

--- src/components/Channels/ChannelWrapper/ChannelWrapper.js
+++ src/components/Channels/ChannelWrapper/ChannelWrapper.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import styled from 'styled-components';
 import Channel from '../Channel/Channel'
 
-const ChannelWrapper = ({channels = []}) => (
-    <StyledWrapper>
-        {channels.map((channel, index) => (
-            <Channel key={index} channel={channel}/>
-        ))}
-    </StyledWrapper>
-);
+const ChannelWrapper = ({channels, handleClick, selectChannel, selected}) => (
+
+	<StyledWrapper>
+		{channels.map((channel, index) => (
+			<Channel
+				selectChannel={selectChannel}
+				selected={selected && selected.link === channel.link}
+				handleClick={handleClick}
+				key={index} channel={channel}/>
+		))}
+	</StyledWrapper>
+)
 
 
 const StyledWrapper = styled.div`
     display: flex;
     color: black;
     flex-direction: column;
-            padding-bottom: 30px;
-            
+    padding-bottom: 30px;  
 `
 
-export default ChannelWrapper;
+
+export default ChannelWrapper
--- src/containers/ChannelsContainer/ChannelContainer.js
+++ src/containers/ChannelsContainer/ChannelContainer.js
@@ -4,7 +4,6 @@ import {bindActionCreators} from 'redux';
 import Ionicon from 'react-ionicons'
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
-const {ipcRenderer} = window.require('electron');
 import {initChannels, changeStatus} from '../../redux/actions/channels'
 import {getOffline, getOnline} from '../../redux/reducers/channels'
 import './style.css';
@@ -12,48 +11,73 @@ import ChannelWrapper from '../../components/Channels/ChannelWrapper/ChannelWrap
 import Channel from '../../components/Channels/Channel/Channel'
 import ChannelForm from '../../components/Channels/ChannelForm/ChannelForm'
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+const {remote, ipcRenderer} = window.require('electron');
+const {Menu, MenuItem} = remote;
+import menuTemplate from '../../helper/menu'
 
 export class ChannelContainer extends Component {
-    constructor() {
-        super()
-    }
+	constructor() {
+		super()
+		this.state = {
+			selected: null,
+		}
+	}
 
-    componentWillMount() {
-        this.props.initChannels()
-    }
+	openMenu = (channel) => {
+		const menu = new Menu();
+		const template = menuTemplate(channel);
+		template.map((item) => menu.append(item))
+		menu.popup(remote.getCurrentWindow())
+	}
 
-    componentDidMount() {
-        ipcRenderer.on('channel-went-online', (event, channel) => this.props.changeStatus(channel.link));
-    }
+	selectChannel = (channel) => {
+		this.setState({selected: channel})
+	}
 
-    render() {
-        const {online, offline} = this.props;
+	componentWillMount() {
+		this.props.initChannels()
+	}
 
-        return (
-            <StyledContainerWrapper>
-                <Tabs>
-                    <TabWrapper>
-                        <TabList className="tabs">
-                            <Tab className='tab' selectedClassName="active">Online ({online.length})</Tab>
-                            <Tab className='tab' selectedClassName="active">Offline ({offline.length})</Tab>
+	componentDidMount() {
+		ipcRenderer.on('channel-went-online', (event, channel) => this.props.changeStatus(channel.link));
+	}
 
-                        </TabList>
-                        <SettingsIcon to="/about"><Ionicon icon="ion-gear-b" color="black"/></SettingsIcon>
-                    </TabWrapper>
-                    <TabPanel className='tab-panel'>
-                        <ChannelWrapper channels={online}/>
-                    </TabPanel>
-                    <TabPanel className='tab-panel'>
-                        <ChannelWrapper channels={offline}/>
-                    </TabPanel>
-                </Tabs>
+	render() {
+		const {online, offline} = this.props;
+		const {selected} = this.state;
+		return (
+			<StyledContainerWrapper>
+				<Tabs>
+					<TabWrapper>
+						<TabList className="tabs">
+							<Tab className='tab' selectedClassName="active">Online ({online.length})</Tab>
+							<Tab className='tab' selectedClassName="active">Offline ({offline.length})</Tab>
 
-                <StyledFooter className="fixed-bottom">
-                    <ChannelForm/>
-                </StyledFooter>
-            </StyledContainerWrapper>
-        );
-    }
+						</TabList>
+						<SettingsIcon to="/about"><Ionicon icon="ion-gear-b" color="black"/></SettingsIcon>
+					</TabWrapper>
+					<TabPanel className='tab-panel'>
+						<ChannelWrapper selected={selected}
+										selectChannel={this.selectChannel}
+										handleClick={this.openMenu}
+										channels={online}
+						/>
+					</TabPanel>
+					<TabPanel className='tab-panel'>
+						<ChannelWrapper
+							selected={selected}
+							selectChannel={this.selectChannel}
+							handleClick={this.openMenu}
+							channels={offline}/>
+					</TabPanel>
+				</Tabs>
+
+				<StyledFooter className="fixed-bottom">
+					<ChannelForm/>
+				</StyledFooter>
+			</StyledContainerWrapper>
+		);
+	}
 }
 const StyledFooter = styled.div`
     background-color: #D7D7D7;
@@ -62,32 +86,32 @@ const StyledFooter = styled.div`
 const SettingsIcon = styled(Link)`
     display: flex;
     justify-content: center;
-        padding-bottom: 30px;
+    padding-bottom: 30px;
 `;
 const StyledContainerWrapper = styled.div`
-  display: flex;
+    display: flex;
     width: 100%;
 `
 
 const StyledChannel = styled(Channel)`
-background-color: yellow
-color: white;
+    background-color: yellow
+    color: white;
 `
 const TabWrapper = styled.div`
-    height:100%;
+     height:100%;
      background-color: #D7D7D7;
      display:flex;
      justify-content: space-between;
      flex-direction: column;
 `
 export default connect(
-    (state) => ({
-        online: getOnline(state),
-        offline: getOffline(state)
-    }),
-    (dispatch) => bindActionCreators({
-        initChannels,
-        changeStatus,
-    }, dispatch)
+	(state) => ({
+		online: getOnline(state),
+		offline: getOffline(state)
+	}),
+	(dispatch) => bindActionCreators({
+		initChannels,
+		changeStatus,
+	}, dispatch)
 )(ChannelContainer);
 
