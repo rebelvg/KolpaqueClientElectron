7f0108f8eb3bace50588055e489720f90c11cd10
Jack Pyro <PyroLovesBuns@gmail.com>
2017-10-08 15:09:21 +0300

    Updated styles

--- src/components/Channels/Channel/Channel.js
+++ src/components/Channels/Channel/Channel.js
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 
 const renderIcon = (service) => {
     if (service === 'twitch') {
-        return <Ionicon fontSize="24px" color="#6441a5" icon="ion-social-twitch-outline"/>
+        return <Ionicon fontSize="16px" color="#6441a5" icon="ion-social-twitch-outline"/>
     }
     else {
-        return <Ionicon icon="ion-eye" color="dark-green"/>
+        return <Ionicon icon="ion-eye" fontSize="16px" color="dark-green"/>
     }
 };
 
@@ -43,18 +43,21 @@ const StyledChannelWrap = styled.div`
     padding: 2px 0px;
     font-weight: bold;
     font-size: 14px;
+    height:25px;
     border-top: 1px solid #969696;
 `;
 
 const StyledIcon = styled.div`
-    margin-right: 10px;
+    margin-right: 5px;
     display: flex;
     align-items: center;
     margin-left: 5px;
+    height:25px
 `;
 
 const StyledName = styled.div`
     flex-grow: 2;
+       height:25px;
 `;
 
 export default Channel;
