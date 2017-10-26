eaf380d87c02d7b4f960fdfa295b4398cd6ce25f
rebelvg <johnhartigan@ya.ru>
2017-10-26 12:05:35 +0300

    Filtering for frontend.

--- src/Channel/Helpers/FilterChannels.js
+++ src/Channel/Helpers/FilterChannels.js
@@ -9,32 +9,40 @@ const FilterChannels = (channels, filter) => {
 
     let filteredChannels = [];
 
-    let filters = filter.split(/\s+/gi);
-
     filteredChannels = _.filter(channels, (channelObj) => {
-        let searchFilters = _.map(filters, (filter) => {
-            return {
-                pattern: filter,
-                found: false
-            };
-        });
-
-        _.forEach([channelObj.link, channelObj.name, channelObj.visibleName], (searchString) => {
-            _.forEach(searchFilters, (filter) => {
-                let regExp = new RegExp(filter.pattern, 'gi');
-
-                if (regExp.test(searchString)) {
-                    filter.found = true;
-                }
-            });
-        });
-
-        return _.filter(searchFilters, 'found').length === filters.length;
+        return FilterChannel(channelObj, filter);
     });
 
-    console.log(filteredChannels);
-
     return filteredChannels;
+};
+
+const FilterChannel = (channelObj, filter) => {
+    filter = filter.trim();
+
+    if (!filter) {
+        return false;
+    }
+
+    let filters = filter.split(/\s+/gi);
+
+    let searchFilters = _.map(filters, (filter) => {
+        return {
+            pattern: filter,
+            found: false
+        };
+    });
+
+    _.forEach([channelObj.link, channelObj.name, channelObj.visibleName], (searchString) => {
+        _.forEach(searchFilters, (filter) => {
+            let regExp = new RegExp(filter.pattern, 'gi');
+
+            if (regExp.test(searchString)) {
+                filter.found = true;
+            }
+        });
+    });
+
+    return _.filter(searchFilters, 'found').length === filters.length;
 };
 
 export default FilterChannels
