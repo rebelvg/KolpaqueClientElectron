bc5756e90c18d109ac99c92ce3e446c38889b010
Volodymyr Rebel <rebelvgchannel@gmail.com>
2021-05-23 13:27:39 +0300

    style fix

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppContainer from './App/Containers/AppContainer';
 import 'normalize.css/normalize.css';
 import './style.css';
+
 const appElement = document.createElement('div');
 
 appElement.setAttribute('id', 'app');
