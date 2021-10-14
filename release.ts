76d6af4c8f6733a63c0731545238973913e95372
Volodymyr Rebel <rebelvgchannel@gmail.com>
2021-10-14 19:21:19 +0300

    add release script

---
+++ release.ts
@@ -0,0 +1,28 @@
+import * as readlineSync from 'readline-sync';
+import * as os from 'os';
+import * as fs from 'fs';
+import * as childProcess from 'child_process';
+
+(() => {
+  const packageJson = JSON.parse(
+    fs.readFileSync('./package.json', { encoding: 'utf-8' }),
+  );
+
+  const newVersion = readlineSync.question(
+    `current version - ${packageJson.version}${os.EOL}enter new version${os.EOL}`,
+  );
+
+  packageJson.version = newVersion;
+
+  fs.writeFileSync('./package.json', JSON.stringify(packageJson));
+
+  childProcess.execSync(`yarn run lint:fix`);
+
+  childProcess.execSync(`git add .`);
+
+  childProcess.execSync(`git commit -m publish version ${newVersion}`);
+
+  childProcess.execSync(`git tag ${newVersion}`);
+
+  childProcess.execSync(`git push --tags`);
+})();
